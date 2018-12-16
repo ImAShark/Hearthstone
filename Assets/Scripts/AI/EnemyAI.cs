a31abce33ab461f1b739af04f31684a07f2c2620
killersshadow <32833605+killersshadow@users.noreply.github.com>
2018-12-16 19:04:18 +0100

    start AI + end turn

---
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    [HideInInspector]
+    public bool myTurn = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (myTurn)
+        {
+            Debug.Log("Ben er klaar voor!");
+        }
+	}
+}
---
+++ Assets/Scripts/EndTurn/EndTurn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTurn : MonoBehaviour {
+
+    public GameObject Enemy;
+    
+    public Sprite Button1;
+    public Sprite Button2;
+    public Button EndButton;
+
+    [HideInInspector]
+    public bool playerTurn = true;
+
+    public void EndThisTurn()
+    {
+        if (playerTurn)
+        {
+            playerTurn = false;
+            EndButton.image.sprite = Button2;
+            Enemy.GetComponent<EnemyAI>().myTurn = true;
+        }        
+        
+    }
+
+}
